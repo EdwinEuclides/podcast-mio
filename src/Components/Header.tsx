1d82512ab31279209b784e447471e1ff7fe51ed7
EdwinEuclides <edwin1egger@hotmail.com>
2024-08-14 20:35:30 -0300

    Heder con los tres componentes

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,12 @@
 import us from "../assets/react.svg"
-
+import logo from '../assets/Logo.svg'
+import Browser from "./Browser"
 export default function Header(){
 
     return(
         <div>
+           <img src= {logo} alt="Logo de la Compañia" />
+            <Browser />
             <img src={us} alt="Usuario" />
         </div>
     )
